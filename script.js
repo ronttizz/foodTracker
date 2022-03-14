3d9b20e725fc5cfdd4925d3feba4318ff4abdfd3
ronttizz <toni.nissinen@gmail.com>
2022-03-14 20:09:39 +0200

    Made some style changes, added placeholders etc

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-const food = document.querySelector("#food");
+// import { FetchWrapper } from "./fetch-wrapper.js";
+
+const foodDetails = document.querySelector("#food");
 const totalCalories = document.querySelector("#total");
+const foodName = document.querySelector("#foodName");
+const carbs = document.querySelector("#carbs");
+const protein = document.querySelector("#protein");
+const fat = document.querySelector("#fat");
+
+const clear = document.querySelector("#clear");
+
+const addFood = (e) => {
+  e.preventDefault();
+  const foodNameString = foodName.value;
+  const carbsValue = carbs.value;
+  const proteinValue = protein.value;
+  const fatValue = fat.value;
+};
+
+foodDetails.addEventListener("submit", addFood);

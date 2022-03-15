9001d811176e6f880d57cbde5c026d3b465ceba6
ronttizz <toni.nissinen@gmail.com>
2022-03-15 17:41:06 +0200

    Implemented FoodAPI for adding food, total calories works

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { FetchWrapper } from "./fetch-wrapper.js";
 
 const foodDetails = document.querySelector("#food");
-const totalCalories = document.querySelector("#total");
+let totalCalories = document.querySelector("#totalCalories");
 const foodList = document.querySelector("#foodsList");
 const foodName = document.querySelector("#foodName");
 const carbs = document.querySelector("#carbs");
@@ -21,14 +21,55 @@ const addFood = (e) => {
   const fatValue = fat.value;
   const calories = carbsValue * 4 + proteinValue * 4 + fatValue * 9;
 
-  const body = {
-    foodName: foodNameString,
-    carbs: carbsValue,
-    protein: proteinValue,
-    fat: fatValue,
-    calories: calories,
-  };
-  addFoodToList(foodNameString, carbsValue, proteinValue, fatValue, calories);
+  if (foodNameString !== "Select select" && carbsValue && proteinValue && fatValue) {
+    const body = {
+      fields: {
+        foodName: {
+          stringValue: foodNameString,
+        },
+        carbs: {
+          integerValue: carbsValue,
+        },
+        protein: {
+          integerValue: proteinValue,
+        },
+        fat: {
+          integerValue: fatValue,
+        },
+        calories: {
+          integerValue: calories,
+        },
+      },
+    };
+    addFoodToList(foodNameString, carbsValue, proteinValue, fatValue, calories);
+    totalCalories.textContent = Number(totalCalories.textContent) + Number(calories);
+    FoodAPI.post("toni112", body);
+    carbs.value = "";
+    protein.value = "";
+    fat.value = "";
+  }
+};
+
+const clearAPI = () => {
+  FoodAPI.get("toni112").then((data) => {
+    for (let index in data.documents) {
+      FoodAPI.delete(`toni112/${data.documents[index].name.slice(67)}`);
+    }
+  });
+};
+
+const updateFoodsList = () => {
+  FoodAPI.get("toni112").then((data) => {
+    for (let i = 0; i < data?.documents?.length; i++) {
+      const fats = data.documents[i].fields.fat.integerValue;
+      const carbs = data.documents[i].fields.carbs.integerValue;
+      const name = data.documents[i].fields.foodName.stringValue;
+      const protein = data.documents[i].fields.protein.integerValue;
+      const calories = data.documents[i].fields.calories.integerValue;
+      addFoodToList(name, carbs, protein, fats, calories);
+      totalCalories.textContent = Number(totalCalories.textContent) + Number(calories);
+    }
+  });
 };
 
 const addFoodToList = (foodNameString, carbsValue, proteinValue, fatValue, calories) => {
@@ -54,4 +95,6 @@ const addFoodToList = (foodNameString, carbsValue, proteinValue, fatValue, calor
   foodList.insertAdjacentHTML("beforeend", element);
 };
 
+updateFoodsList();
+clear.addEventListener("click", clearAPI);
 foodDetails.addEventListener("submit", addFood);

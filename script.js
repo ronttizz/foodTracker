027d1dd6107bf6be810b43ed6e064efade5f1a3e
ronttizz <toni.nissinen@gmail.com>
2022-03-15 09:57:25 +0200

    Added missing calories to added food

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// import { FetchWrapper } from "./fetch-wrapper.js";
+import { FetchWrapper } from "./fetch-wrapper.js";
 
 const foodDetails = document.querySelector("#food");
 const totalCalories = document.querySelector("#total");
@@ -7,6 +7,9 @@ const foodName = document.querySelector("#foodName");
 const carbs = document.querySelector("#carbs");
 const protein = document.querySelector("#protein");
 const fat = document.querySelector("#fat");
+const FoodAPI = new FetchWrapper(
+  "https://firestore.googleapis.com/v1/projects/programmingjs-90a13/databases/(default)/documents/"
+);
 
 const clear = document.querySelector("#clear");
 
@@ -18,23 +21,36 @@ const addFood = (e) => {
   const fatValue = fat.value;
   const calories = carbsValue * 4 + proteinValue * 4 + fatValue * 9;
 
-  addFoodToList(foodNameString, carbsValue, proteinValue, fatValue);
+  const body = {
+    foodName: foodNameString,
+    carbs: carbsValue,
+    protein: proteinValue,
+    fat: fatValue,
+    calories: calories,
+  };
+  addFoodToList(foodNameString, carbsValue, proteinValue, fatValue, calories);
 };
 
-const addFoodToList = (foodNameString, carbsValue, proteinValue, fatValue) => {
+const addFoodToList = (foodNameString, carbsValue, proteinValue, fatValue, calories) => {
   let element = `<div class="foodListItem">
-    <div>
-      <h3>${foodNameString}</h2>
-    </div>
-    <div>
-      <p>Carbs: <span id="foodCarbs">${carbsValue}</span>g</p>
-    </div>
-    <div>
-      <p>Protein: <span id="foodProtein">${proteinValue}</span>g</p>
-    </div>
-    <div>
-      <p>Fat: <span id="foodFat">${fatValue}</span>g</p>
-    </div>`;
+  <div>
+    <h3>${foodNameString}</h2>
+  </div>
+  <div>
+    <p class="small">Calories: <span id="calories">${calories}</span>kcal</p>
+  </div>
+  <div class="macro">
+      <div>
+      <p>Carbs: <span class="foodCarbs">${carbsValue}</span>g</p>
+      </div>
+      <div>
+      <p>Protein: <span class="foodProtein">${proteinValue}</span>g</p>
+      </div>
+      <div>
+      <p>Fat: <span class="foodFat">${fatValue}</span>g</p>
+      </div>
+  </div>
+</div>`;
   foodList.insertAdjacentHTML("beforeend", element);
 };
 
